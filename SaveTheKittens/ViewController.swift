4c2ee061d990449e910f80c06bbefdaae2fcdc2d
Vuk Knežević <Vuk@Admin-Mac-mini.local>
2018-12-29 01:17:48 +0100

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Vuk Knežević on 12/29/18.
+//  Copyright © 2018 Teodora Knežević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
